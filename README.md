a79e01012e0717bd8497240fd8a127524a5fed01
Harsh-Singh09 <harshrsingh552005@gmail.com>
2024-04-29 21:22:28 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# netflix_slider_api
+ In this repo,I am creating netflix like slider using real api 
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <>
+      <h1 className="text-red-500">hiii</h1>
+    </>
+  );
+};
+
+export default App;
